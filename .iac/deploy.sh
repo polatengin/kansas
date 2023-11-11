4cf42e264529742a0fc03f0f2c9609dcf8b3ceef
polatengin <enpolat@microsoft.com>
2023-11-11 21:54:56 +0000

    installing telepresence if it doesn't exists on the local computer

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -2,6 +2,13 @@ LOCATION=${2:-eastus}
 
 PROJECT_NAME=${1:-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)}
 
+if ! command -v telepresence &> /dev/null
+then
+  echo "telepresence could not be found, installing..."
+  sudo curl -fL https://app.getambassador.io/download/tel2oss/releases/download/v2.16.1/telepresence-linux-amd64 -o /usr/local/bin/telepresence
+  sudo chmod a+x /usr/local/bin/telepresence
+fi
+
 az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
 
 az acr create --resource-group "rg-${PROJECT_NAME}" --name "acr${PROJECT_NAME}" --sku "Basic"
