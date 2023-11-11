0792aca46f39682a077ee645f7d327e39765238b
polatengin <enpolat@microsoft.com>
2023-11-11 22:26:42 +0000

    adding tag variable

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -2,6 +2,8 @@ LOCATION=${2:-eastus}
 
 PROJECT_NAME=${1:-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)}
 
+TAG=$(git rev-parse --short HEAD)
+
 az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
 
 az acr create --resource-group "rg-${PROJECT_NAME}" --name "acr${PROJECT_NAME}" --sku "Basic"
