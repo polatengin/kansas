1901009201068cbd281d8335b93468cafa2f4d56
polatengin <enpolat@microsoft.com>
2023-11-11 21:46:13 +0000

    creating azure container registry

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -3,3 +3,5 @@ LOCATION=${2:-eastus}
 PROJECT_NAME=${1:-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)}
 
 az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
+
+az acr create --resource-group "rg-${PROJECT_NAME}" --name "acr${PROJECT_NAME}" --sku "Basic"
