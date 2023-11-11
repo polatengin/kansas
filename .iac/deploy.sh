f9463dbaafdc53a33bac79807a4b8985139161d9
polatengin <enpolat@microsoft.com>
2023-11-11 21:46:52 +0000

    creating azure kubernetes instance

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -7,3 +7,5 @@ az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
 az acr create --resource-group "rg-${PROJECT_NAME}" --name "acr${PROJECT_NAME}" --sku "Basic"
 
 az acr login --name "acr${PROJECT_NAME}"
+
+az aks create --resource-group "rg-${PROJECT_NAME}" --name "aks-${PROJECT_NAME}" --node-count 1 --generate-ssh-keys --attach-acr "acr${PROJECT_NAME}"
